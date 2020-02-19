d4dcadb3c02a835fb72917108c8e1859cf0d69d5
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2020-02-20 00:57:37 +0530

    added kuch to

--- src/main/App.java
+++ src/main/App.java
@@ -1,7 +1,17 @@
 package main;
 
+import user.Person;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("hello World.");
+        Scanner scanner = new Scanner(System.in);
+        int serialNumber = scanner.nextInt();
+        String firstName = scanner.nextLine().trim();
+        String lastName = scanner.nextLine().trim();
+        String emailAddress = scanner.nextLine().trim();
+        Person person = new Person()
     }
 }
---
+++ src/user/Person.java
@@ -0,0 +1,29 @@
+package user;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Person {
+    // a field to store the first name
+    // a field to store the last name
+    // a list to store multiple contact numbers (i.e a list of numbers)
+    // a field to store the email address of the person
+
+    private int serialNumber;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private LinkedList <String> contacts = null;
+
+    //constructor
+    public Person(int serialNumber, String firstName, String lastName, String emailAddress, LinkedList<String> contacts) {
+        this.serialNumber = serialNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.contacts = contacts;
+    }
+
+
+}
+
